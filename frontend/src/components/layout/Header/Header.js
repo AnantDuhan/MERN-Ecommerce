2dcf408356fb0827524c51a6946909086cbfaedb
AnantDuhan <duhananant@gmail.com>
2022-04-12 19:35:37 +0530

    Added Icons in Header

--- frontend/src/components/layout/Header/Header.js
+++ frontend/src/components/layout/Header/Header.js
@@ -1,12 +1,15 @@
-import React from 'react';
 import { ReactNavbar } from 'overlay-navbar';
+import React from 'react';
+import { FaSearch, FaShoppingCart, FaUserAlt } from 'react-icons/fa';
+
 import logo from '../../../images/logo.png';
 
 const options = {
-    burgerColorHover: '#eb4034',
     logo,
     logoWidth: '20vmax',
     navColor1: 'white',
+    navColor2: '#0b111e',
+    burgerColorHover: '#eb4034',
     logoHoverSize: '10px',
     logoHoverColor: '#eb4034',
     link1Text: 'Home',
@@ -18,18 +21,26 @@ const options = {
     link3Url: '/contact',
     link4Url: '/about',
     link1Size: '1.3vmax',
-    link1Color: 'rgba(35, 35, 35,0.8)',
+    link1Color: 'white',
     nav1justifyContent: 'flex-end',
     nav2justifyContent: 'flex-end',
     nav3justifyContent: 'flex-start',
     nav4justifyContent: 'flex-start',
     link1ColorHover: '#eb4034',
     link1Margin: '1vmax',
+    profileIcon: true,
+    ProfileIconElement: FaUserAlt,
     profileIconUrl: '/login',
-    profileIconColor: 'rgba(35, 35, 35,0.8)',
-    searchIconColor: 'rgba(35, 35, 35,0.8)',
-    cartIconColor: 'rgba(35, 35, 35,0.8)',
+    profileIconColor: 'white',
     profileIconColorHover: '#eb4034',
+    searchIconColor: 'white',
+    cartIcon: true,
+    CartIconElement: FaShoppingCart,
+    CartIconUrl: '/cart',
+    cartIconColor: 'white',
+    searchIcon: true,
+    SearchIconElement: FaSearch,
+    SearchIconUrl: '/search',
     searchIconColorHover: '#eb4034',
     cartIconColorHover: '#eb4034',
     cartIconMargin: '1vmax',
