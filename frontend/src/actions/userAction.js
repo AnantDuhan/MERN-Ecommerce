954f2d3abc99e9a2a32174d86ecbbda2f270b85b
AnantDuhan <duhananant@gmail.com>
2022-04-13 11:29:39 +0530

    Added 2 constants, reducers, actions

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,15 +1,27 @@
 import axios from 'axios';
 
-import { CLEAR_ERRORS, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS } from '../constants/userConstants';
+import {
+    CLEAR_ERRORS,
+    LOGIN_FAIL,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    REGISTER_USER_FAIL,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
+} from '../constants/userConstants';
 
 // Login
 export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({ type: LOGIN_REQUEST });
 
-        let link = `/api/v1/login`;
-        const config = { headers: { 'Content-Type': 'application/json' } };
-        const { data } = await axios.post(link, { email, password }, config);
+        const config = { headers: { "Content-Type" : "application/json" } };
+        const { data } = await axios.post(`/api/v1/login`, { email, password }, config);
         dispatch({ type: LOGIN_SUCCESS, payload: data.user });
     } catch (error) {
         dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
@@ -21,9 +33,12 @@ export const register = (userData) => async (dispatch) => {
     try {
         dispatch({ type: REGISTER_USER_REQUEST });
 
-        const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+        const config = {
+            headers: { 'Content-Type': 'multipart/form-data' },
+        };
 
-        const { data } = await axios.post('/api/v1/register', userData, config);
+        console.log({ userData });
+        const { data } = await axios.post(`/api/v1/register`, userData, { config });
 
         dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
     } catch (error) {
@@ -33,6 +48,40 @@ export const register = (userData) => async (dispatch) => {
         });
     }
 };
+
+// load user 
+export const loadUser = () => async (dispatch) => {
+    try {
+        dispatch({
+            type: LOAD_USER_REQUEST,
+        });
+
+        const { data } = await axios.get(
+            `/api/v1/me`,
+        );
+        dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
+    } catch (error) {
+        dispatch({
+            type: LOAD_USER_FAIL,
+            payload: error.response.data.message,
+        });
+    }
+};
+
+// logout User
+export const logout = () => async (dispatch) => {
+    try {
+
+        await axios.get(`/api/v1/logout`);
+        dispatch({ type: LOGOUT_SUCCESS });
+    } catch (error) {
+        dispatch({
+            type: LOGOUT_FAIL,
+            payload: error.response.data.message,
+        });
+    }
+};
+
 
 // clearing errors
 export const clearErrors = () => async (dispatch) => {
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -8,7 +8,7 @@ import {
     CLEAR_ERRORS,
 } from '../constants/productConstants';
 
-export const productReducer = (state = { products: [] }, action) => {
+export const productsReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case ALL_PRODUCT_REQUEST:
             return {
