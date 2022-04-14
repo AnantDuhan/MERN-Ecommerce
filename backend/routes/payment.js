ada3a27378afb58c8b8c2f8ee5b8c8ba0b77ea6a
AnantDuhan <duhananant@gmail.com>
2022-04-15 01:18:55 +0530

    Added payment controller & routes

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
     processPayment,
     sendStripeApiKey,
-} = require('../controllers/paymentController');
+} = require('../controllers/payment');
 const router = express.Router();
 const { isAuthUser } = require('../middleware/auth');
 
