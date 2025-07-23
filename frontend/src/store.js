5bc551b48e09322c0b4d475a3b37d9c73b1e148e
Anant Duhan <anantduhan@Anants-MacBook-Air.local>
2025-07-24 00:02:52 +0530

    install @redux-devtools/extension

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
 
 import { cartReducer } from './reducers/cartReducer';
